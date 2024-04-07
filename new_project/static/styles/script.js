0a3db5861ed0cc9416de7158d85779ae4f0bee91
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-04-07 16:37:51 +0000

    password visibility done

--- new_project/static/styles/script.js
+++ new_project/static/styles/script.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById("popup-open").addEventListener('click', function() {
     document.getElementById("popup").classList.add("open");
 })
-
 document.getElementById("popup-close").addEventListener('click', function() {
     document.getElementById("popup").classList.remove("open");
 })
@@ -30,3 +29,20 @@ document.getElementById("popup").addEventListener('click', event => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove("open");
 });
+
+//password
+function changeVis (event) {
+    event.preventDefault();
+    let icon1 = document.getElementById("icon1");
+    let password1 = document.getElementById("password1");
+
+    if (password1.type == "password") {
+        password1.type = "text";
+        icon1.classList.remove("fa-eye-slash");
+        icon1.classList.add("fa-eye");
+    } else {
+        password1.type = "password";
+        icon1.classList.remove("fa-eye");
+        icon1.classList.add("fa-eye-slash");
+    }
+}
